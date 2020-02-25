a9bfb4c0a1da38b3c1ad09883174fcf90fc28f7a
Dimitar Mitkov <dimitar.mitkov@riscogroup.com>
2020-02-25 13:43:08 +0200

    Completed
    The game is done. Future commits can fix bugs, if there are any.

--- memory_game/README.md
+++ memory_game/README.md
@@ -1,9 +1,3 @@
 # Memory Game
 
-The purpose of this game is to successfully find all matching images. There are two cards of every type (16 total). At
-the start of the game random places for each card is chosen and saved. After that player should be able to click the different
-slots. When he click, the card that is selected to be on this place is displayed. When he clicks a different slot, it opens
-as well. If the two images match, they stay open, if they don't match, then those images should be hidden again. Player wins
-after all images are face up.
-
-If you have any questions regarding the task, or if you need help, don't hesitate to contact me.
+Future improvements: Cache the images and all the resources needed for the webpage. This way, once loaded, the game can run until it is closed. The restart button should take this into account and not simply reload the page. Hence, the game will achieve connection indepency.
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,34 @@
 const $document = $(document);
 
 function image_click (array_fields, field) {
-	// Load image
-	let id = array_fields[Number(field.id)];
-	field.append(`<img src="images/back/10.jpeg" />`);
-	// field.remove($("img"));
+	let id = array_fields[Number(field.id) - 1];
+	let current_field = $(field);
 
+	// Load image
+	current_field.append(`<img src='images/pairs/${id}.jpg' id='${id}' class='picture' />`);
+	// Remove the back
+	current_field.find(".back").remove();
+
+	// Return the image id
 	return array_fields[Number(field.id) - 1];
+}
+
+function unload_image (field) {
+	let id = field.id;
+	let current_field = $(field);
+
+	// Load the back
+	current_field.append(`<img src='images/back/${id}.jpeg' id='${id}' class='back' />`);
+	// Remove the image
+	current_field.find(".picture").remove();
+
+	// Return empty
+	return 0;
+}
+
+function reset () {
+	// Reload the page
+	location.reload();
 }
 
 function initialize_field () {
@@ -46,24 +68,64 @@ function main () {
 
 	let first_image = 0;
 	let second_image = 0;
+	let first_field = 0;
+	let second_field = 0;
+	let can_click_flag = true;
 
-	$('.image').click(function () {
-		if (first_image != 0) {
+	$(".box").click(function () {
+		// Block insane people with their clicks everywhere
+		if (!can_click_flag) {
+			return;
+		}
+		can_click_flag = false;
 
+		if (first_field == 0) {
+			// Populate the first field
+			first_field = this;
+			// Show the picture
+			first_image = image_click(array_fields, first_field);
+			can_click_flag = true;
+		} else {
+			// Populate the second field
+			second_field = this;
 		}
 
-		if (first_image == 0) {
-			first_image = image_click (array_fields, this);
-		} else {
-			second_image = image_click (array_fields, this);
-			if (first_image != second_image) {
-				// Delay 1 sec
-				// Unload images
+		// Is the same field clicked again?
+		if (second_field == first_field) {
+			// Reset the field
+			first_image = unload_image(first_field);
+			first_field = 0;
+			second_field = 0;
+			can_click_flag = true;
+		} else if (second_field != 0){
+			// Show the picture
+			second_image = image_click(array_fields, second_field);
+			if (second_image == first_image) {
+				// Matching pictures stay visible and they cannot be clicked anymore
+				$(first_field).off("click");
+				$(second_field).off("click");
+				// Reset the parameters so the player can continue
+				first_field = 0;
 				first_image = 0;
+				second_field = 0;
 				second_image = 0;
+				can_click_flag = true;
+			} else {
+				// The pictures do not match. Reset the fields after one second
+				setTimeout(function() {
+					second_image = unload_image(second_field);
+					first_image = unload_image(first_field);
+					first_field = 0;
+					second_field = 0;
+					can_click_flag = true;
+				}, 1000);
 			}
 		}
+	});
 
+	// Reset button
+	$("#reset-game").click(function() {
+		reset();
 	});
 }
 
